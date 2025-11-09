c141bde2a6df4a5c37b6315f0a4a58d12f222c50
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-11-09 07:41:11 -0500

    Corriger connexion.php

--- api/utilisateurs/Connexion.php
+++ api/utilisateurs/Connexion.php
@@ -1,15 +1,11 @@
 <?php
-echo 'sssss';
-exit();
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
-
 
 // require_once __DIR__ . "/../config/database.php";
 
 // Récupérer les données JSON envoyées
 $data = json_decode(file_get_contents("php://input"));
-
 if (empty($data->nom) || empty($data->mdp)) {
     http_response_code(400);
     echo json_encode([
@@ -19,14 +15,14 @@ if (empty($data->nom) || empty($data->mdp)) {
 }
 
 if ($data->nom == "admin" && $data->mdp == "12345") {
-    http_response_code(response_code: 200);
+    http_response_code(200);
     echo json_encode([
         "utilisateur" => "admin",
         "message" => "Connexion réussie"
     ]);
     exit();
 } else {
-    http_response_code(response_code: 401);
+    http_response_code(401);
     echo json_encode([
         "message" => "Infos de connexion incorrectes"
     ]);
